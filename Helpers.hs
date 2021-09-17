9b40280362eec9c6875fad5b0f1a99a0a50d00fe
Joe Teague <joeteague@live.com>
2021-09-18 01:54:40 +1000

    Did a bunch more state stuff for envelope states.  I don't think the state monad stuff should be as hardwired in as it is currently, should probably be replaced with Lens

--- Helpers.hs
+++ Helpers.hs
@@ -1,7 +1,61 @@
 module Helpers where
 
+import Prelude hiding (unzip)
+import Control.Monad.State
+import Data.Tuple.Extra
+import Control.Functor.HT (unzip)
   
 rot :: Int -> [a] -> [a]
 rot _ [] = []
 rot 0 xs = xs
 rot n (x:xs) = rot (n-1) $ xs ++ [x]
+
+mapWhere :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+mapWhere p f = map (\x -> if (p x) then f x else x)
+
+
+(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+(.:) = fmap . fmap
+
+
+
+
+
+
+
+
+
+
+-- State things
+-- ==================================
+
+-- joinStatesWith :: (a -> b -> c) -> State s1 a -> State s2 b -> State (s1, s2) c
+-- joinStatesWith f op1 op2 = state $ \(s1, s2) -> let
+--     (out1, s1') = runState op1 s1
+--     (out2, s2') = runState op2 s2
+--     in (f out1 out2, (s1', s2'))
+
+idState :: State a ()
+-- idState = state $ \x -> ((), x)
+idState = modify id
+
+pairStatesWith ::  (a -> b -> c) -> State s1 a -> State s2 b -> State (s1, s2) c
+pairStatesWith f op1 op2 = state $ \s -> let
+    result = runState op1 *** runState op2 $ s
+    out = uncurry f . (fst *** fst) $ result
+    s' = snd *** snd $ result
+    in (out, s')
+
+
+-- from Prelude or Data.List
+-- unzip :: [(a,b)] -> ([a], [b])
+stateMap :: State s a -> State [s] [a]
+stateMap op = state $ unzip . map (runState op)
+
+-- from Data.Functor.HT
+-- unzip :: Function f => f (a,b) -> (f a, f b)
+injectState :: Functor f => State s a -> State (f s) (f a)
+injectState op = state $ unzip . fmap (runState op)
+
+injectWith :: Functor f => (f a -> a) -> State s a -> State (f s) a
+injectWith combine op = state $ first combine . unzip . fmap (runState op)
--- hasynth.hs
+++ hasynth.hs
@@ -95,73 +95,21 @@ performSequenceTET keyHz synth scale notes = concat $ map makeChord notes
         sq = 60.0/tempo/4.0
 
 
+-- =================================================================================
+-- =================================================================================
+-- =================================================================================
+-- =================================================================================
 
 
--- #V1 do it all at once :(
--- --          startTime  note
--- type Voice = (Seconds, NoteNumber)
--- type InstrumentState = (Seconds, [Voice])
-
--- -- #TODO how will we put envelopes in here
--- performMidiSequence :: ChannelNumber -> Waveform -> MidiSequence -> [Pulse]
--- performMidiSequence channel osc messages = [0.0]
---     where
---         runSynth :: [Seconds] -> Voice -> [Pulse]
---         runSynth ts (startTime, num) = map (osc . (*hz)) tsRelative
---             where
---                 tsRelative = map (\t -> t-startTime) ts
---                 hz = hzFromNoteNumber num
-
---         stepSynths :: Seconds -> State InstrumentState [Pulse]
---         stepSynths dt = do
---             (t, voices) <- get
---             let ts = map ((+t) . (/sampleRate)) $ tail [0.0..dt*sampleRate]
---             let sounds = map (runSynth ts) voices
---             return (map sum $ transpose sounds)
-
---         readMessage :: MidiMessage -> State InstrumentState [Pulse]
---         readMessage (Broadcast dt) = stepSynths dt
---         -- readMessage (ChannelMessage dt ch event) 
---         --         | ch /= channel = stepSynths dt
---         --         | otherwise = do
---         --             case event of
---         --                 NoteOn num vel -> modify (\(t, voices) -> (t, (t,num):voices))
---         --                 NoteOff num -> modify (second $ filter $ \(_, n) -> n /= num)
---         --                 PitchBend val -> undefined
---         --             stepSynths dt
---         readMessage (ChannelMessage dt ch event) = do
---             if ch == channel
---             then case event of
-
---                     NoteOn num vel -> modify (\(t, voices) -> (t, (t,num):voices))
---                     NoteOff num -> modify (second $ filter $ \(_, n) -> n /= num)
---                     PitchBend val -> undefined
---             else pure ()
---             stepSynths dt
+-- #TODO this probably doesn't all need to be in the state monad
+--      Lens might be better...
 
 
-
--- #TODO envelopes are going to need to be able to do something when noteOff happens...
 type Volume = Float
+
+
 type VolumeEnvelope = Either ([Volume], [Volume]) [Volume]
 type FrequencyEnvelope = ([Hz], [Hz])
-
--- -- #V2 inject (State Voice) into State [Voice]
--- type Voice = (Phase, Waveform, Hz, VolumeEnvelope)
--- data Voice = Voice {
---     phase :: Phase,
---     wave :: Waveform,
---     freq :: Hz,
---     vol :: VolumeEnvelope
--- }
-
--- stepVoice :: Seconds -> State Voice Pulse
--- stepVoice dt = state $ run
---     where
---         run voice = case vol voice of
---             Left (v:vs, [decay]) -> (v * (wave $ phase + dt * freq), voice {vol=Left (vs, decay), phase = phase+freq*dt})
---             Right v:vs -> (v * (wave $ phase + dt * freq), voice {vol=Right vs, phase = phase+freq*dt})
-
 
 type OscState = (Waveform, Phase, Hz)
 type VoiceState = (OscState, VolumeEnvelope)
@@ -171,14 +119,20 @@ stepOscillator dt = state $ \(wave, phase, hz) -> (wave $ phase + dt*hz, (wave, 
 
 stepEnvelope :: Seconds -> State VolumeEnvelope Volume
 stepEnvelope dt = state $ \venv -> case venv of
+    Left ([v], decay) -> (v, Left ([v], decay))
     Left (v:vs, decay) -> (v, Left (vs, decay))
+    Left ([], decay) -> (1.0, Left ([], decay))
     Right (d:ds) -> (d, Right ds)
+    Right ([]) -> (0.0, Right [])
+
 
 noteOffEnv :: State VolumeEnvelope ()
 noteOffEnv = state $ \venv -> case venv of
     Left (_, decay) -> ((), Right decay)
     Right decay -> ((), Right decay)
-    
+
+
+
 
 -- stepVoice :: Seconds -> State VoiceState Pulse
 -- stepVoice dt = state $ \(osc, vol) -> let
@@ -187,40 +141,132 @@ noteOffEnv = state $ \venv -> case venv of
 --     in (v*val, (osc', vol'))
 
 stepVoice :: Seconds -> State VoiceState Pulse
-stepVoice dt = joinStatesWith (*) (stepOscillator dt) (stepEnvelope dt)
+stepVoice dt = pairStatesWith (*) (stepOscillator dt) (stepEnvelope dt)
 
 voiceOff :: State VoiceState ()
 voiceOff = state $ \(osc, vol) -> ((), (osc, snd $ runState noteOffEnv vol))
 
 
--- joinStatesWith :: State s1 a -> State s2 b -> (a -> b -> c) -> State (s1, s2) c
--- joinStatesWith op1 op2 f = state $ \(s1, s2) -> let
---     (out1, s1') = runState op1 s1
---     (out2, s2') = runState op2 s2
---     in (f out1 out2, (s1', s2'))
 
-joinStatesWith ::  (a -> b -> c) -> State s1 a -> State s2 b -> State (s1, s2) c
-joinStatesWith f op1 op2 = state $ \s -> let
-    result = runState op1 *** runState op2 $ s
-    out = uncurry f . (fst *** fst) $ result
-    s' = snd *** snd $ result
-    in (out, s')
+-- =================================================================================
+-- =================================================================================
+    
+-- what a F** mess
+
+-- #TODO give this an enum for currentState
+data VolEnv1 = VolEnv1 {
+        attackSlope :: Float,
+        decaySlope :: Float,
+        sustainLevel :: Float,
+        releaseSlope :: Float,
+        currentState :: Int,
+        volume :: Volume
+    }
+data OscState1 = OscState1 {
+    wave :: Waveform,
+    phase :: Phase,
+    freq :: Hz
+}
+
+-- #TODO a voice should be allowed to mix multiple oscillators... 
+type VoiceState1 = (OscState1, VolEnv1)
+
+-- #TODO should maybe be called an instrument 
+type SynthState1 = ([VoiceState1], [NoteNumber]) 
 
 
--- from Prelude or Data.List
--- unzip :: [(a,b)] -> ([a], [b])
-enlist :: State s a -> State [s] [a]
-enlist op = state $ unzip . map (runState op)
 
--- from Data.Functor.HT
--- unzip :: Function f => f (a,b) -> (f a, f b)
-injectState :: Functor f => State s a -> State (f s) (f a)
-injectState op = state $ unzip . fmap (runState op)
+voiceFromNote :: NoteNumber -> VoiceState1
+voiceFromNote note = undefined
 
-injectWith :: Functor f => (f a -> a) -> State s a -> State (f s) a
-injectWith combine op = state $ first combine . unzip . fmap (runState op)
+stepOsc1 :: Seconds -> State OscState1 Pulse
+-- stepOsc1 dt = state $ \(wave, phase, hz) -> (wave $ phase + dt*hz, (wave, phase + dt*hz, hz))
+stepOsc1 dt = state $ \osc -> let newPhase = phase osc + dt*(freq osc)
+                                in (wave osc $ newPhase, osc {phase = newPhase})
+
+restartEnv1 :: VolEnv1 -> VolEnv1
+restartEnv1 venv = venv { currentState = 0}
+
+restartEnvVoice1 :: VoiceState1 -> VoiceState1
+restartEnvVoice1 = second restartEnv1
+
+-- This is super messy :(
+stepEnv1 :: Seconds -> State VolEnv1 Volume
+stepEnv1 dt = state $ \venv -> case currentState venv of
+    -- #TODO what if attackSlope is 0
+    0 -> let nextVol = volume venv + (attackSlope venv) * dt 
+             overStep = dt - (1 - volume venv) / (attackSlope venv)
+        in  if nextVol <= 1.0 
+            then (nextVol, venv {volume = nextVol}) 
+            else runState (stepEnv1 overStep) (venv { volume=1.0, currentState = 1})
+    -- #TODO end of decay needs to go to sustain
+    1 -> let nextVol = volume venv - (decaySlope venv) * dt 
+             overStep = dt - (volume venv - sustainLevel venv)/(decaySlope venv)
+        in if nextVol >= (sustainLevel venv)
+            then (nextVol, venv {volume = nextVol})
+            else runState (stepEnv1 overStep) (venv { volume=sustainLevel venv, currentState = 2})        
+    2 -> (volume venv, venv) -- sustain, nothing changes
+    3 -> let nextVol = volume venv - (releaseSlope venv) * dt 
+             overStep = dt - (volume venv) / (releaseSlope venv)
+        in if nextVol >= 0
+            then (nextVol, venv {volume = nextVol})
+            else (0.0, venv {currentState = 4})
+    4 -> (0, venv) -- stopped, ready for GC
 
 
+
+-- jump to decay section of envelope
+noteOffEnv1 :: VolEnv1 -> VolEnv1
+noteOffEnv1 venv = venv {currentState=3}
+-- state can be made with:
+-- noteOffEnv1 :: State VolEnv1 ()
+-- noteOffEnv1 = modify noteOffEnv1
+
+stepVoice1 :: Seconds -> State VoiceState1 Pulse
+stepVoice1 dt = pairStatesWith (*) (stepOsc1 dt) (stepEnv1 dt)
+
+-- #TODO should voice have NoteNumber, so this takes note number and does nothign if they don't match?
+noteOffVoice1 :: State VoiceState1 ()
+noteOffVoice1 = pairStatesWith const idState (modify noteOffEnv1)
+
+
+-- const :: b -> a -> b
+-- flip const :: a -> b -> b
+-- -- for synth :: [voice]
+-- stepSynth1 :: State SynthState1 Pulse
+-- -- stepSynth1 = state $ \synth ->  let (pulses, states) = stateMap stepVoice1 synth
+-- --                                     states' = filter ((<4) . currentState . second) states
+-- --                                 in (sum pulses, states')
+-- stepSynth1 = fmap sum $ stateMap stepVoice1
+
+-- for synth :: ([voice], [notenumber])
+stepSynth1 :: Seconds -> State SynthState1 Pulse
+stepSynth1 dt = state $ \(voices, notes) ->  
+    let (pulses, steppedVoices) = runState (stateMap $ stepVoice1 dt) voices
+        -- running :: (VoiceState1, NoteNumber) -> Boolean
+        -- running :: ((OscState1, VolEnv1), NoteNumber) -> Boolean
+        running = ((<4) . currentState . snd . fst) 
+        states' = unzip $ filter running $ zip steppedVoices notes
+    in (sum pulses, states')
+
+-- #TODO how do I keep track of which voices go with which notes?  
+--  instead of [Voice] have map MidiNote -> Voice?
+--  [Voice] and [MidiNote]?
+noteOnSynth1 :: NoteNumber -> State SynthState1 ()
+-- if there are any voices for that note, set the envelope to state 0
+--  otherwise add a new voice for the note
+noteOnSynth1 note = modify $ \(voices, notes) -> 
+    let states :: [(VoiceState1, NoteNumber)]
+        states = zip voices notes
+    in if any (== note) notes
+        then unzip $ mapWhere ((==note) . snd) (first (second  restartEnv1)) states  --revert envelope to state 1
+        else ((voiceFromNote note):voices, note:notes) --add a new voice for that note
+    
+
+-- first :: (a -> c) -> (a,b) -> (c,b)
+-- second :: (b -> c) -> (a,b) -> (a,c)
+
+-- first . second :: (b -> d) -> ((a,b), c) -> ((a,d), c)
 
 
 -- =====================================================
@@ -233,7 +279,7 @@ main = do
     putStrLn $ "made " ++ filename
 
 -- B.floatLE is float little endian
-song = B.toLazyByteString $ mconcat $ map B.floatLE wave
+song = B.toLazyByteString $ mconcat $ map B.floatLE output
 
 printSong :: IO ()
 printSong = putStrLn $ concatMap ((++" ") . show) $ zip scaleDegrees freqs
@@ -241,15 +287,15 @@ printSong = putStrLn $ concatMap ((++" ") . show) $ zip scaleDegrees freqs
         scaleDegrees = [-7..21]
         freqs = map ionian19TET scaleDegrees
 
-wave :: [Pulse]
--- wave = performSequence defaultSynth dorian tonalCenter jump
--- wave = performSequence defaultSynth ionian tonalCenter silentNight
--- wave = jumpTour
--- wave = jumpTour2
--- wave = silentNightFull locrian
-wave = silentNightFull thiccIonian
--- wave = jumpFull phrygian
--- wave = performSequence pureSynth ionian tonalCenter silentNightChords
+output :: [Pulse]
+-- output = performSequence defaultSynth dorian tonalCenter jump
+-- output = performSequence defaultSynth ionian tonalCenter silentNight
+-- output = jumpTour
+-- output = jumpTour2
+-- output = silentNightFull locrian
+output = silentNightFull thiccIonian
+-- output = jumpFull phrygian
+-- output = performSequence pureSynth ionian tonalCenter silentNightChords
 
 
 silentNightFull :: Scale -> [Pulse]
